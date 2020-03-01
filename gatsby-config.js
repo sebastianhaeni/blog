ba00e8b278feaf3d0be4e132db663c178462ef2b
Sebastian Häni <haeni.sebastian@gmail.com>
2020-03-01 18:28:00 +0100

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-blog.git)

---
+++ gatsby-config.js
@@ -0,0 +1,84 @@
+module.exports = {
+  pathPrefix: "/blog",
+  siteMetadata: {
+    title: `Blog of Sebastian Häni`,
+    author: {
+      name: `Sebastian Häni`,
+      summary: `who lives, studies and works Zurich building useful things.`,
+    },
+    description: `A personal blog about things I am interested in.`,
+    siteUrl: `https://sebastianhaeni.github.com/blog/`,
+    social: {
+      twitter: `sebastianhaeni`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sebastian Häni Blog`,
+        short_name: `Sebastian Häni`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
