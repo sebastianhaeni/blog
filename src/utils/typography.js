379d899e4b5ae7a0ee4543b4ea9e9e9a4efea7a6
Sebastian Häni <haeni.sebastian@gmail.com>
2020-04-27 00:49:56 +0200

    fix code styles being too large

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,6 @@
 import Typography from "typography"
 import LincolnTheme from "typography-theme-lincoln"
 
-LincolnTheme.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-    '.gatsby-highlight': {
-      fontSize: "0.7em",
-    },
-  }
-}
-
 delete LincolnTheme.googleFonts
 
 const typography = new Typography(LincolnTheme)
