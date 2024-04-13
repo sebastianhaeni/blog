4f5a12387d9a4c281274726cd624d44fbea529ab
Sebastian Häni <haeni.sebastian@gmail.com>
2024-04-13 17:10:30 +0200

    Gatsby deploy

---
+++ component---src-pages-404-js-480c1a4dd32e38cdc8ef.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksebastianhaeni_blog=self.webpackChunksebastianhaeni_blog||[]).push([[125],{3331:function(t,e,a){a.r(e);var n=a(6540),l=a(3895),s=a(2269);e.default=t=>{let{data:e,location:a}=t;const i=e.site.siteMetadata.title;return n.createElement(l.A,{location:a,title:i},n.createElement(s.A,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-480c1a4dd32e38cdc8ef.js.map
